a23b49d508c9357b5e203d1efcbae46d71b8e709
MarkCapka <61602557+MarkCapka@users.noreply.github.com>
2021-11-29 11:15:59 -0800

    untested skybox code
    will push to main once i have tested with an image. Right now it will just play the "hello world" example as I didn't call the skybox without an associated image.

---
+++ skyboxJavaFX/src/main/java/com/capstone/skyboxjavafx/HelloController.java
@@ -0,0 +1,17 @@
+package com.capstone.skyboxjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+
+        //TODO is this where I need to put calls and setup for skybox?
+
+    }
+}
