7de8a7f3d053744634a5563338763971d480eefb
anthonehi <78658590+anthonehi@users.noreply.github.com>
2022-03-07 12:03:35 -0800

    Working sunpath and boolean

--- skyboxJavaFX/SkyBoxJavaFX-Tester/src/main/java/module-info.java
+++ skyboxJavaFX/SkyBoxJavaFX-Tester/src/main/java/module-info.java
@@ -1,14 +1,13 @@
 module com.example.skyboxjavafxtester {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires jim3dsModelImporterJFX;
 
     opens com.example.skyboxjavafxtester to javafx.fxml;
     exports com.example.skyboxjavafxtester;
